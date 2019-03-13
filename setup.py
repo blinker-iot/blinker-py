12fbebdf387931919fd39494abcb05932efa6f7a
i3water <121024123@qq.com>
2019-03-13 15:53:38 +0800

    update codes, upgrade BLINKER PYTHON LIB requirement.txt

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ setup(
     packages=['Blinker', 'BlinkerAdapters', 'BlinkerUtility'],
     # package_dir={'Blinker':'', 'BlinkerAdapters':'', 'BlinkerUtility':''},
     url='https://github.com/blinker-iot/blinker-py',
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    license='MIT',
     description='Blinker library in python',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ]
 )
