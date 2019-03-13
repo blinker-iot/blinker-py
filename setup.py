0548f2cf8601b40a422325d251d36ff76610292e
i3water <121024123@qq.com>
2019-03-13 16:59:34 +0800

    update codes, upgrade setup.py codes

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 
 setup(
     name='Blinker',
     version='0.2.0',
     author='i3water',
+    description='Blinker library in python',
+    long_description=open('README.md').read(),
+    license='MIT',
+    url='https://github.com/blinker-iot/blinker-py',
     packages=['Blinker', 'BlinkerAdapters', 'BlinkerUtility'],
     # package_dir={'Blinker':'', 'BlinkerAdapters':'', 'BlinkerUtility':''},
-    url='https://github.com/blinker-iot/blinker-py',
     zip_safe=False,
     include_package_data=True,
     platforms='any',
-    license='MIT',
-    description='Blinker library in python',
-    long_description=open('README.md').read(),
+    install_requires=[
+        "SimpleWebSocketServer",
+        "zeroconf",
+        "paho-mqtt",
+        "requests",
+        "dbus-python",
+        "pygobject",
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
