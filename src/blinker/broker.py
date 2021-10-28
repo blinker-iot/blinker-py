cb55db9c3e53acc821dbf077ebb83570ce89dada
stao <werewolf_st@hotmail.com>
2021-10-29 00:07:00 +0800

    设备上线，云心跳，心跳，基本的mqtt和http通信调整

--- src/blinker/broker.py
+++ src/blinker/broker.py
@@ -3,7 +3,10 @@
 
 __author__ = "stao"
 
-import demjson
+import json
+
+import ssl
+import certifi
 
 from typing import Dict
 from loguru import logger
@@ -14,6 +17,9 @@ from paho.mqtt.client import Client
 from errors import BlinkerBrokerException
 
 __all__ = ["Broker"]
+
+ssl_context = ssl.create_default_context()
+ssl_context.load_verify_locations(certifi.where())
 
 
 def generate_broker_info(broker, client_id, product_key="") -> Dict:
@@ -38,6 +44,7 @@ def generate_broker_info(broker, client_id, product_key="") -> Dict:
 class Broker:
     def __init__(self, broker: str, client_id: str, product_key: str = "", host: str = "", port: int = "",
                  username: str = "", password: str = ""):
+        self.clientId = client_id
         self.client = Client(client_id=client_id)
         broker_info = generate_broker_info(broker, client_id, product_key)
         self._sub_topic = broker_info["sub_topic"]
@@ -56,59 +63,28 @@ class Broker:
     def _on_connect(self, client, userdata, flags, rc):
         logger.info("Connect to broker from mqtt")
         if rc == 0:
-            logger.info("Connected")
+            logger.success("Broker connected...")
             self.client.subscribe(self._sub_topic)
-            logger.info("Subscribe mqtt success")
         else:
-            logger.info("Connect to broker error, code is {0}".format(rc))
+            logger.error("Connect to broker error, code is {0}".format(rc))
 
     def _on_message(self, client, userdata, msg):
-        logger.info("Received message:")
-        logger.info('Subscribe topic: {0}'.format(msg.topic))
-        logger.info('payload: {0}'.format(msg.payload))
-
-        get_topic = msg.topic[0:55]
-        message_id = msg.topic[55:]
-
-        logger.info('Get topic: {0}'.format(get_topic))
-        logger.info('message_id: {0}'.format(message_id))
-
-        data = msg.payload
-        # if get_topic != self.bmqtt.exatopic:
-        data = data.decode('utf-8')
-        data = demjson.decode(data)
-        from_device = data['fromDevice']
-        data = data['data']
-        # data = json.dumps(data)
-
-        logger.info('data: {0}'.format(data))
-
-        forward_msg = {"fromDevice": from_device, "msg": data}
-
-        self.received_msg.append(forward_msg)
+        received_msg = msg.payload.decode("utf8")
+        self.received_msg.append(received_msg)  # str
 
     def connection(self):
-        logger.info("Host: {0}".format(self.host))
-        logger.info("Port: {0}".format(self.port))
-
         self.client.username_pw_set(self.username, self.password)
         self.client.on_connect = self._on_connect
         self.client.on_message = self._on_message
 
         if self.protocol == "mqtts:":
-            self.client.tls_set()
+            self.client.tls_set_context(context=ssl_context)
         self.client.connect_async(self.host, int(self.port))
-
-    def run(self):
         self.client.loop_start()
 
-    def stop(self):
-        self.client.loop_stop()
-
-    def pub(self, payload):
-        logger.info(self.client.is_connected())
-        logger.info("Mqtt push msg: {0}".format(payload))
+    def pub(self, to_device, data):
         if not self.client.is_connected():
-            self.connection()
-        res = self.client.publish(self._pub_topic, payload)
-        logger.info("Mqtt pushed result: {0}".format(res.is_published()))
+            self.client.reconnect()
+
+        payload = json.dumps({"fromDevice": self.clientId, "toDevice": to_device, "data": data})
+        self.client.publish(self._pub_topic, payload)
--- src/blinker/device.py
+++ src/blinker/device.py
@@ -4,6 +4,8 @@
 __author__ = "stao"
 
 import asyncio
+import json
+
 from loguru import logger
 
 from _http import *
@@ -13,7 +15,7 @@ __all__ = ["Device"]
 
 
 class BaseDevice(object):
-    def __init__(self, authkey: str, version: str = "1.0", protocol: str = "mqtts", ali_type: str = None,
+    def __init__(self, authkey: str, version: str = "1.0", protocol: str = "mqtt", ali_type: str = None,
                  duer_type: str = None, mi_type: str = None):
         self.authKey = authkey
         self.version = version
@@ -27,34 +29,40 @@ class BaseDevice(object):
 
         self.push_broker_msg = None
 
+        self.widgets = {}  # {key: widget}
+
+    async def add_widget(self, widget):
+        self.widgets[widget.key] = widget
+
+    async def handle_widget_data(self, key, data):
+        return self.widgets[key].func(data)
+
     async def init_actions(self):
         self.device = await device_auth(self.authKey, self.version, self.protocol, self.aliType, self.duerType,
                                         self.miType)
 
-        logger.success("device auth")
+        logger.success("device auth ok...")
 
     async def waiting_auth_device(self):
         while True:
             if self.device:
                 break
-            logger.warning("waiting auth device...")
+            logger.info("waiting auth device...")
             await asyncio.sleep(2)
 
     async def waiting_broker_connected(self):
         while True:
             if self.broker:
                 break
-            logger.warning("waiting broker connected...")
+            logger.info("waiting broker connected...")
             await asyncio.sleep(2)
 
     async def init_broker(self):
         await self.waiting_auth_device()
         self.broker = Broker(self.device["broker"], self.device["deviceName"], self.device["productKey"],
                              self.device["host"], self.device["port"], self.device["iotId"], self.device["iotToken"])
-        self.broker.connection()
-        logger.debug("broker status: {0}".format(self.broker.client.is_connected()))
         self.push_broker_msg = self.broker.pub
-        logger.success("Broker is connected")
+        self.broker.connection()
 
     async def heartbeat(self):
         await self.waiting_auth_device()
@@ -62,20 +70,19 @@ class BaseDevice(object):
         while True:
             await send_heartbeat(self.device["deviceName"], self.authKey)
             logger.info("Send heartbeat")
-            await asyncio.sleep(10)
+            await asyncio.sleep(600)
 
     async def handle_data(self):
         await self.waiting_broker_connected()
         while True:
             if not self.broker.received_msg:
-                logger.info("waiting for mqtt msg....")
                 await asyncio.sleep(10)
                 continue
-            received_msg = self.broker.received_msg.pop()
+            received_msg = json.loads(self.broker.received_msg.pop())
             logger.info("received msg: {0}".format(received_msg))
 
             target_device = received_msg["fromDevice"]
-            received_data = received_msg["msg"]
+            received_data = received_msg["data"]
 
             if isinstance(received_data, (str, int)):
                 logger.info("not json data: {0}".format(received_data))
@@ -90,7 +97,6 @@ class BaseDevice(object):
         asyncio.ensure_future(self.heartbeat())
         asyncio.ensure_future(self.init_broker())
         asyncio.ensure_future(self.handle_data())
-        print("task3")
 
     def run(self):
         self.task()
